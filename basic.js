394606afae51a479fe40df2f6612872842361246
Neltuzz <ninjal@rumsan.com>
2023-05-25 14:10:53 +0545

    initial

---
+++ basic.js
@@ -0,0 +1,13 @@
+let addition = 0;
+
+let x = 1;
+let y = 10000000000870;
+addition = x + y;
+let multiplication = x * y;
+let subtraction = x - y;
+let division = x / y
+
+console.log("addition", addition);
+console.log("multiplication", multiplication)
+console.log('subtraction', subtraction)
+console.log('division', division)
---
+++ practice.js
@@ -0,0 +1,12 @@
+let y = 21;
+let b = 79;
+
+let addition = b + y;
+let multiplication = b * y;
+let subtraction = b - y;
+let division = b / y;
+
+console.log('addition', addition);
+console.log("multiplication", multiplication);
+console.log('subtraction', subtraction);
+console.log('division', division)
